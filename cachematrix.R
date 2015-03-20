fb386f758836dc63be41b23ec77cb5e24bd60da3
Hoe Kit CHEW <hoekit@gmail.com>
2015-03-20 10:59:09 +0700

    Working, tested version of functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+##
+## cachematrix.R
+##
+## Matrix inversion is usually a costly computation and there may be some
+## benefit to caching the inverse of a matrix rather than compute it
+## repeatedly. This script contains a pair of functions that cache the
+## inverse of a matrix.
+##
+################################################################################
 
-## Write a short comment describing this function
-
+## makeCacheMatrix:  create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+    inv <- NULL
+
+    # Returns cached matrix inverse
+    cachedInverse <- function() inv
+
+    # Returns plain matrix data
+    matrixData <- function() x
+
+    # Stores matrix inverse in cache
+    storeInverse <- function(inverse) inv <<- inverse
+
+    list( cachedInverse = cachedInverse,
+          matrixData = matrixData,
+          storeInverse = storeInverse )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: computes the inverse of the special "matrix" returned by
+## makeCacheMatrix. If the inverse has already been calculated (and the matrix
+## has not changed), then cachesolve will retrieve the inverse from the cache.
+## Assumption: matrix supplied is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    inv <- x$cachedInverse()    # Get the stored inverse from cache
+
+    if (!is.null(inv)) {        # Return cached inverse if exists
+        message("getting cached data")
+        return(inv)
+    }
+
+    # Compute the matrix inverse
+    inv <- solve(x$matrixData())
+
+    # Store computed inverse into the cache
+    x$storeInverse(inv)
+
+    # Return computed inverse
+    inv
+
 }
+
